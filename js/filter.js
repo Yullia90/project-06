2a4540cc2134f0a3c240684dba855393114ab1e4
Yullia90 <y.tsarenko@harwind.com.ua>
2023-02-28 23:26:31 +0200

    add

---
+++ js/filter.js
@@ -0,0 +1,11 @@
+const filterBox = document.querySelectorAll('.box');
+document.querySelector('nav').addEventListener('click', event => {
+  if (event.target.tagName !== 'button') return false;
+  let filterClass = event.target.dataset['f'];
+  filterBox.forEach(elem => {
+    elem.classList.remove('hide');
+    if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
+      elem.classList.add('hide');
+    }
+  });
+});
